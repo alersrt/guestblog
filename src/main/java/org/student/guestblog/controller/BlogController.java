9139d8c5f09bec92d9efa7d66d225430d5e4bb14
alersrt <strtmg@yandex.ru>
2017-08-23 01:21:13 +0700

    # interim commint

--- src/main/java/org/student/guestblog/controller/BlogController.java
+++ src/main/java/org/student/guestblog/controller/BlogController.java
@@ -2,9 +2,13 @@ package org.student.guestblog.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.student.guestblog.model.Message;
 import org.student.guestblog.service.MessageService;
 
 @Controller
@@ -14,9 +18,18 @@ public class BlogController {
 	private MessageService messageService;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ModelAndView welcome(ModelAndView model) {
+	public ModelAndView main(ModelAndView model) {
 		model.addObject("listMessages", messageService.findAll());
 		model.setViewName("main");
 		return model;
+	}
+
+	@RequestMapping(value = "add", method = RequestMethod.POST)
+	public String add(@ModelAttribute("messageForm") Message messageForm, BindingResult bindingResult, Model model) {
+		if (bindingResult.hasErrors()) {
+			return "add";
+		}
+		messageService.save(messageForm);
+		return "redirect:/";
 	}
 }
