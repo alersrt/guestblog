d096e45f21525d0890dda7387bb661142a63c582
alersrt <strtmg@yandex.ru>
2018-03-16 23:41:15 +0700

    Start of working at the new version

--- src/main/java/org/student/guestblog/service/SecurityService.java
+++
@@ -1,6 +0,0 @@
-package org.student.guestblog.service;
-
-public interface SecurityService {
-	String findLoggedInUsername();
-	void autoLogin(String username, String password);
-}
--- src/test/java/org/student/guestblog/service/AbstractServiceTest.java
+++
@@ -1,8 +0,0 @@
-package org.student.guestblog.service;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class AbstractServiceTest {
-	final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
-}
