a04491750ed1100974d955401a7a7dd1b05c2508
alersrt <strtmg@yandex.ru>
2018-05-04 22:02:15 +0700

    Organize imports in Application.java

--- src/main/java/org/student/guestblog/Application.java
+++ src/main/java/org/student/guestblog/Application.java
@@ -1,8 +1,9 @@
 package org.student.guestblog;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.slf4j.Slf4j;
 
 /** The main class of the application. */
 @Slf4j
