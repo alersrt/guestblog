9e987012ed07bdd6dafb1fab9d1349d21d48d443
alersrt <strtmg@yandex.ru>
2018-05-05 03:20:14 +0700

    Use tomcat:8.5 image in Dockerfile

--- src/main/java/org/student/guestblog/config/DataConfig.java
+++ src/main/java/org/student/guestblog/config/DataConfig.java
@@ -1,9 +1,7 @@
 package org.student.guestblog.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 /** JavaConfig for Data. */
 @Configuration
-@EnableMongoRepositories(basePackages = "org.student.guestblog.repository")
 public class DataConfig {}
