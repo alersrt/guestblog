759d9b4265eeaf085b67b233c7f82494e8589433
alersrt <strtmg@yandex.ru>
2018-05-19 06:08:31 +0700

    Rewrite and extend unit tests for GET /posts/

--- src/test/java/org/student/guestblog/rest/GeneralControllerTest.java
+++ src/test/java/org/student/guestblog/rest/GeneralControllerTest.java
@@ -1,41 +1,39 @@
 package org.student.guestblog.rest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.student.guestblog.entity.Post;
 import org.student.guestblog.repository.PostRepository;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
+import lombok.extern.slf4j.Slf4j;
+
 /** Unit test for {@link GeneralController}. */
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@Slf4j
 @DisplayName("Test of the REST endpoints")
 @Tag("unit")
-public class GeneralControllerTest {
-
-  /** Rest client for the testing. */
-  @Autowired
-  private TestRestTemplate restTemplate;
+class GeneralControllerTest {
 
   /** Mocked {@link PostRepository}. */
-  @MockBean
-  private PostRepository postRepository;
+  private PostRepository postRepository = mock(PostRepository.class);
+
+  /** An object of the {@link GeneralController}. */
+  private GeneralController generalController = new GeneralController(postRepository, new Gson());
 
   /** Executes before each test. */
   @BeforeEach
@@ -45,7 +43,8 @@ public class GeneralControllerTest {
 
   /** Executes after each test. */
   @AfterEach
-  void tearDown() {}
+  void tearDown() {
+  }
 
   @DisplayName("GET '/posts/': exception")
   @Test
@@ -54,12 +53,44 @@ public class GeneralControllerTest {
     when(postRepository.findAll()).thenThrow(new RuntimeException("Something went wrong"));
 
     /*------ Actions ------*/
-    ResponseEntity<JsonObject> answer = restTemplate.getForEntity("/api/posts", JsonObject.class);
+    ResponseEntity<JsonObject> answer = generalController.posts();
 
     /*------ Asserts ------*/
     assertThat(answer.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
     assertThat(answer.getBody().get(Protocol.ERROR_NAME).getAsString()).isNotEmpty();
     assertThat(answer.getBody().get(Protocol.ERROR_DESCRIPTION).getAsString()).isNotEmpty();
+  }
+
+  @DisplayName("GET '/posts/': returns 204 code")
+  @Test
+  void posts_empty() {
+    /*------ Arranges ------*/
+    when(postRepository.findAll()).thenReturn(new ArrayList<>());
+
+    /*------ Actions ------*/
+    ResponseEntity<JsonObject> answer = generalController.posts();
+
+    /*------ Asserts ------*/
+    assertThat(answer.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+  }
+
+  @DisplayName("GET '/posts/': returns 200 code")
+  @Test
+  void posts_notEmpty() {
+    /*------ Arranges ------*/
+    String postId = "mockedPost0";
+    List<Post> posts = new ArrayList<>();
+    Post post = new Post();
+    post.setId(postId);
+    posts.add(post);
+    when(postRepository.findAll()).thenReturn(posts);
+
+    /*------ Actions ------*/
+    ResponseEntity<JsonObject> answer = generalController.posts();
+
+    /*------ Asserts ------*/
+    assertThat(answer.getStatusCode()).isEqualTo(HttpStatus.OK);
+    assertThat(answer.getBody().get(postId)).isNotNull();
   }
 }
 
