3057f4ad960b575877219fd63ac85a91e132de0e
alersrt <strtmg@yandex.ru>
2018-06-09 05:20:50 +0700

    Implements adding and deliting of posts

--- src/main/webapp/static/js/webapp.js
+++ src/main/webapp/static/js/webapp.js
@@ -3,17 +3,26 @@ const instanceAxios = axios.create({
   timeout: 10000,
 });
 
+onLoad();
+
 function onLoad() {
   instanceAxios.get('/posts/').then(function(response) {
     let posts = response.data['posts'];
 
     posts.forEach(function(p) {
       ReactDOM.render(
-        <div id={p.id}>
-          <div>{p.title}</div>
-          <div>{p.text}</div>
+        <div class="card-columns">
+          <div id={p.id} className="card border rounded">
+            <div className="card-body">
+              <div className="card-header">{p.title}</div>
+              <div className="card-text">{p.text}</div>
+              <div className="card-footer">
+                <button className="btn btn-secondary" onClick={() => delPost(p.id)}>Delete</button>
+              </div>
+            </div>
+          </div>
         </div>,
-        document.getElementById('root')
+        document.getElementById('root'),
       );
     });
   }).catch(function(error) {
@@ -21,4 +30,28 @@ function onLoad() {
   });
 }
 
-onLoad();
+function addPost() {
+  instanceAxios.put('/posts/', {
+    title: document.getElementById('post-title').value,
+    text: document.getElementById('post-text').value,
+  }).
+  then(function () {
+    $('#add-post').modal('hide');
+    onLoad();
+    console.log(document.getElementById('post-title').textContent)
+  }).catch(function(error) {
+    console.log(error);
+  });
+}
+
+function delPost(id) {
+  instanceAxios.delete('/posts/', {
+    data: {
+      id: id,
+    },
+  }).then(function () {
+    onLoad()
+  }).catch(function(error) {
+    console.log(error);
+  });
+}
