1f9c42022d2e58166ad075d142910cd0bd47bec5
alersrt <strtmg@yandex.ru>
2018-08-11 05:11:50 +0700

    Setup model's relation between user and messages

--- src/main/java/org/student/guestblog/model/Message.java
+++ src/main/java/org/student/guestblog/model/Message.java
@@ -2,9 +2,12 @@ package org.student.guestblog.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDateTime;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 /** Represents information about post. */
@@ -20,10 +23,12 @@ public class Message {
 
   /** Title of the post. */
   @JsonProperty("title")
+  @Size(max = 100)
   private String title;
 
   /** Body of the post. */
   @JsonProperty("text")
+  @Size(max = 1024)
   private String text;
 
   /** Attachment. Name of the file in GridFS. */
@@ -32,9 +37,15 @@ public class Message {
 
   /** Time when this post was created or edited. */
   @JsonProperty("timestamp")
+  @NotNull
   private LocalDateTime timestamp;
 
   /** Flag determines was this post edited or no. */
   @JsonProperty("isEdited")
   private boolean isEdited;
+
+  /** Author of this post. */
+  @JsonProperty("user")
+  @DBRef(db = "user")
+  private User user;
 }
--- src/main/java/org/student/guestblog/model/User.java
+++ src/main/java/org/student/guestblog/model/User.java
@@ -1,25 +1,81 @@
 package org.student.guestblog.model;
 
-import lombok.Builder;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.userdetails.UserDetails;
 
 /** Represents information about user which is stored in the database. */
 @Data
-@Builder
+@NoArgsConstructor
+@RequiredArgsConstructor
 @Document
-public class User {
+public class User implements UserDetails {
 
   /** Id of the user. */
-  @Id private String id;
+  @JsonProperty("id")
+  @Id
+  private String id;
 
   /** Username of the user. */
+  @JsonProperty("username")
+  @NotBlank
+  @Size(min = 6, max = 32)
+  @NonNull
   private String username;
 
   /** Hash code of the user password. */
+  @JsonProperty("password")
+  @NotBlank
+  @NonNull
   private String password;
 
   /** E-mail of the user. */
+  @JsonProperty("email")
+  @Email
   private String email;
+
+  /** List of the user's messages. */
+  @JsonIgnore
+  @DBRef(db = "message")
+  private List<Message> messages;
+
+  /** Authorities of this user. */
+  @JsonIgnore
+  private List<String> authorities;
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return username;
+  }
 }
