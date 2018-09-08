1c4ee137a801762f64f62e6f6faa35147fb1c732
alersrt <strtmg@yandex.ru>
2018-09-08 17:40:59 +0700

    Fix cyclic bean dependencies at security configuration

--- src/main/java/org/student/guestblog/config/SecurityConfig.java
+++ src/main/java/org/student/guestblog/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package org.student.guestblog.config;
 
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,15 +20,15 @@ import org.student.guestblog.service.UserService;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-  @Autowired
+  @Setter(onMethod = @__(@Autowired))
   private ApplicationAuthenticationProvider authenticationProvider;
 
-  @Autowired
+  @Setter(onMethod = @__(@Autowired))
   private ApplicationSecurityContextRepository securityContextRepository;
 
-  @Autowired
+  @Setter(onMethod = @__(@Autowired))
   private UserService userService;
 
   /**
@@ -40,31 +41,41 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     return new BCryptPasswordEncoder(11);
   }
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http
-      .httpBasic().disable()
-      .formLogin().disable()
-      .logout().disable()
-      .csrf().disable()
+  /**
+   * Returns configured {@link WebSecurityConfigurerAdapter}.
+   *
+   * @return {@link WebSecurityConfigurerAdapter} bean.
+   */
+  @Bean
+  public WebSecurityConfigurerAdapter webSecurityConfigurerAdapter() {
+    return new WebSecurityConfigurerAdapter() {
+      @Override
+      protected void configure(HttpSecurity http) throws Exception {
+        http
+          .httpBasic().disable()
+          .formLogin().disable()
+          .logout().disable()
+          .csrf().disable()
 
-      .userDetailsService(userService)
-      .authenticationProvider(authenticationProvider)
+          .userDetailsService(userService)
+          .authenticationProvider(authenticationProvider)
 
-      .securityContext()
-      .securityContextRepository(securityContextRepository)
+          .securityContext()
+          .securityContextRepository(securityContextRepository)
 
-      .and()
-      .sessionManagement()
-      .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+          .and()
+          .sessionManagement()
+          .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
-      .and()
-      .authorizeRequests()
-      .antMatchers(HttpMethod.GET).permitAll()
-      .antMatchers(
-        "/api/users/register",
-        "/api/users/sign/in",
-        "/api/users/current"
-      ).permitAll();
+          .and()
+          .authorizeRequests()
+          .antMatchers(HttpMethod.GET).permitAll()
+          .antMatchers(
+            "/api/users/register",
+            "/api/users/sign/in",
+            "/api/users/current"
+          ).permitAll();
+      }
+    };
   }
 }
