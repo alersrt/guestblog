0c2f8a82fa2a05ae1339df399417d40361ccc451
alersrt <strtmg@yandex.ru>
2018-09-20 13:59:16 +0700

    Allow CORS for any addresses

--- src/main/java/org/student/guestblog/rest/controller/FileController.java
+++ src/main/java/org/student/guestblog/rest/controller/FileController.java
@@ -1,6 +1,7 @@
 package org.student.guestblog.rest.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.student.guestblog.service.internal.FileResource;
 @RequestMapping("/api/files")
 public interface FileController {
 
+  @CrossOrigin(origins = "*")
   @GetMapping("/{filename}")
   ResponseEntity<FileResource> getFile(@PathVariable String filename);
 }
--- src/main/java/org/student/guestblog/rest/controller/UserController.java
+++ src/main/java/org/student/guestblog/rest/controller/UserController.java
@@ -1,6 +1,7 @@
 package org.student.guestblog.rest.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,12 +17,15 @@ import org.student.guestblog.rest.dto.user.UserResponse;
 @RequestMapping("/api/users")
 public interface UserController {
 
+  @CrossOrigin(origins = "*")
   @GetMapping("/current")
   ResponseEntity<UserResponse> currentUser();
 
+  @CrossOrigin(origins = "*")
   @PostMapping("/register")
   ResponseEntity<RegisterResponse> register(@RequestBody RegisterRequest registerRequest);
 
+  @CrossOrigin(origins = "*")
   @PostMapping("/sign/in")
   ResponseEntity<AuthResponse> signIn(@RequestBody AuthRequest authRequest);
 }
