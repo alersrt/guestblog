30660322d6991d1d9f90258979de8ee6b803b17e
Aleksandr <strtmg@gmail.com>
2024-03-02 01:41:57 +0700

    3.3.0

--- src/main/java/org/student/guestblog/security/HazelcastTokenRepository.java
+++ src/main/java/org/student/guestblog/security/HazelcastTokenRepository.java
@@ -4,12 +4,17 @@ import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.map.IMap;
+import com.hazelcast.nio.serialization.compact.CompactReader;
+import com.hazelcast.nio.serialization.compact.CompactSerializer;
+import com.hazelcast.nio.serialization.compact.CompactWriter;
 import org.springframework.security.web.authentication.rememberme.PersistentRememberMeToken;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.stereotype.Component;
 
+import java.time.ZoneId;
 import java.util.Date;
 import java.util.Map;
+import javax.annotation.Nonnull;
 
 /**
  * Token repository with Hazelcast backend.
@@ -24,7 +29,37 @@ public class HazelcastTokenRepository implements PersistentTokenRepository {
     public HazelcastTokenRepository() {
         ClientConfig clientConfig = new ClientConfig();
         clientConfig.setClusterName("gb-hazelcast");
-        clientConfig.getNetworkConfig().addAddress("localhost", "localhost:5701");
+        clientConfig
+            .getNetworkConfig()
+            .addAddress("localhost", "localhost:5701");
+        clientConfig
+            .getSerializationConfig()
+            .setAllowOverrideDefaultSerializers(true)
+            .getCompactSerializationConfig()
+            .addSerializer(new CompactSerializer<Date>() {
+                @Nonnull
+                @Override
+                public Date read(@Nonnull CompactReader compactReader) {
+                    return Date.from(compactReader.readTimestamp("date").atZone(ZoneId.systemDefault()).toInstant());
+                }
+
+                @Override
+                public void write(@Nonnull CompactWriter compactWriter, @Nonnull Date date) {
+                    compactWriter.writeTimestamp("date", date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime());
+                }
+
+                @Nonnull
+                @Override
+                public String getTypeName() {
+                    return Date.class.getTypeName();
+                }
+
+                @Nonnull
+                @Override
+                public Class<Date> getCompactClass() {
+                    return Date.class;
+                }
+            });
 
         client = HazelcastClient.newHazelcastClient(clientConfig);
     }
