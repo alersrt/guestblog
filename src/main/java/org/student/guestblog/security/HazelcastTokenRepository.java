e06c84894d923a3a30fa33aef02c43ee4ca1629b
Aleksandr <strtmg@gmail.com>
2024-03-02 01:23:32 +0700

    3.3.0

--- src/main/java/org/student/guestblog/security/HazelcastTokenRepository.java
+++ src/main/java/org/student/guestblog/security/HazelcastTokenRepository.java
@@ -1,10 +1,15 @@
 package org.student.guestblog.security;
 
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.map.IMap;
 import org.springframework.security.web.authentication.rememberme.PersistentRememberMeToken;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.Map;
 
 /**
  * Token repository with Hazelcast backend.
@@ -12,23 +17,44 @@ import java.util.Date;
 @Component
 public class HazelcastTokenRepository implements PersistentTokenRepository {
 
+    private final HazelcastInstance client;
+
+    private final String MAP_NAME = "persistent_logins";
+
+    public HazelcastTokenRepository() {
+        ClientConfig clientConfig = new ClientConfig();
+        clientConfig.setClusterName("gb-hazelcast");
+        clientConfig.getNetworkConfig().addAddress("localhost", "localhost:5701");
+
+        client = HazelcastClient.newHazelcastClient(clientConfig);
+    }
+
     @Override
     public void createNewToken(PersistentRememberMeToken token) {
-
+        Map<String, PersistentRememberMeToken> tokens = client.getMap(MAP_NAME);
+        tokens.put(token.getSeries(), token);
     }
 
     @Override
     public void updateToken(String series, String tokenValue, Date lastUsed) {
-
+        Map<String, PersistentRememberMeToken> tokens = client.getMap(MAP_NAME);
+        var token = tokens.get(series);
+        if (token != null) {
+            tokens.replace(series, token, new PersistentRememberMeToken(token.getUsername(), series, tokenValue, lastUsed));
+        }
     }
 
     @Override
     public PersistentRememberMeToken getTokenForSeries(String seriesId) {
-        return null;
+        Map<String, PersistentRememberMeToken> tokens = client.getMap(MAP_NAME);
+        return tokens.get(seriesId);
     }
 
     @Override
     public void removeUserTokens(String username) {
-
+        IMap<String, PersistentRememberMeToken> tokens = client.getMap(MAP_NAME);
+        tokens.removeAll(entry -> entry.getValue() != null
+            && entry.getValue().getUsername() != null
+            && entry.getValue().getUsername().equals(username));
     }
 }
