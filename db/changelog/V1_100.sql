f89d556d13cad99a92079b262f2ceba13ab09156
Aleksandr <strtmg@gmail.com>
2024-06-28 22:33:16 +0700

    Update infrastructure
    - Change PostgreSQL settings

--- db/changelog/V1_100.sql
+++ db/changelog/V1_100.sql
@@ -1,6 +1,15 @@
+create extension pg_stat_statements;
+
+create user gb_user password 'gb_user';
+create user gb_user_rw password 'gb_user_rw';
+
+alter database gbdb owner to gb_user;
+
+grant connect on database gbdb to gb_user_rw;
+revoke connect on database gbdb from public;
+
 create schema if not exists gbsm authorization gb_user;
+
 grant usage on schema gbsm to gb_user_rw;
-alter user gb_user set default_tablespace = gb_tbs;
 alter user gb_user set search_path = gbsm,public;
-alter user gb_user_rw set default_tablespace = gb_tbs;
 alter user gb_user_rw set search_path = gbsm,public;
