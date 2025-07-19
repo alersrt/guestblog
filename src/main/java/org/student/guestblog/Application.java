d25bb2a06e0cbe474879e05bf6abfc3131e3a529
Aleksandr <strtmg@gmail.com>
2025-07-19 16:40:21 +0700

    Start switching to Micronaut

--- src/main/java/org/student/guestblog/Application.java
+++ src/main/java/org/student/guestblog/Application.java
@@ -1,16 +1,14 @@
 package org.student.guestblog;
 
+import io.micronaut.runtime.Micronaut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.lang.invoke.MethodHandles;
 
 /**
  * The main class of the application.
  */
-@SpringBootApplication(scanBasePackages = "org.student.*")
 public class Application {
 
     private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
@@ -22,7 +20,7 @@ public class Application {
      */
     public static void main(String... args) {
         log.info("*** Starting application...");
-        SpringApplication.run(Application.class, args);
+        Micronaut.run(Application.class, args);
         log.info("*** Application started...");
     }
 }
